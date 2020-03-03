81f19ca013998de8a5859a3331c336b9a70a7e92
Sivaprakash.B <sivaprakash674@gmail.com>
2020-03-03 18:26:44 +0530

    Adding Frames for Formating or Alignment Changes

--- logpad.py
+++ logpad.py
@@ -4,6 +4,11 @@ m=Tkinter.Tk()
 m.title('LogPad')
 filename =""
 
+topframe = Tkinter.Frame(m)
+topframe.pack( side = Tkinter.TOP )
+bottomframe = Tkinter.Frame(m)
+bottomframe.pack( side = Tkinter.BOTTOM )
+
 def RunButton():
      global filename
      filename = tkFileDialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
@@ -14,7 +19,6 @@ def RunButton():
 def RunButton2():
      global filename,logtype,cataegory
      logarea.delete('1.0', Tkinter.END)
-     logarealabel.config(text = "Log Area")
      if cataegory.get() :
          if logtype.get() == "all" :
             searchquery = "awk '{ print $6;}' "+ filename +" | sort -u"
@@ -30,87 +34,92 @@ def RunButton2():
 
 # Select File action button creation and packing to the view area
 
-button = Tkinter.Button(m, text='Select File', bg='green',fg='white', command=RunButton)
+button = Tkinter.Button(topframe, text='Select File', bg='green',fg='white', command=RunButton)
 button.pack(fill="none")
 
 # Get Logs action button creation and packing to the view area
 
-button2 = Tkinter.Button(m, text='Get Logs', bg='green',fg='white', command=RunButton2)
+button2 = Tkinter.Button(topframe, text='Get Logs', bg='green',fg='white', command=RunButton2)
 button2.pack(fill="none")
 
 # File Path label creation and packing to the view area
 
-filepathlabel = Tkinter.Label(m,justify = Tkinter.LEFT)
+filepathlabel = Tkinter.Label(topframe,justify = Tkinter.LEFT)
 filepathlabel.pack(anchor=Tkinter.W)
 
 # File Path packing to the view area
 
-filepatharea = Tkinter.Text(m,wrap=Tkinter.WORD,width=200, height= 2)
+filepatharea = Tkinter.Text(topframe,wrap=Tkinter.WORD,width=200, height= 2)
 filepatharea.pack(fill="none", expand=Tkinter.TRUE)
 
 # Search box label creation and packing to the view area
 
-searchboxlabel = Tkinter.Label(m,justify = Tkinter.LEFT)
-searchboxlabel.pack(anchor=Tkinter.E)
+searchboxlabel = Tkinter.Label(topframe,justify = Tkinter.LEFT)
+searchboxlabel.pack(side=Tkinter.LEFT)
 searchboxlabel.config(text = "Search Area")
  
 # Search box text area creation and packing to the view area
 
-searchbox=Tkinter.Text(m, height=2, width=50)
-searchbox.pack(anchor=Tkinter.E)
+searchbox=Tkinter.Text(topframe, height=2, width=50, borderwidth=2, relief=Tkinter.GROOVE)
+searchbox.pack(side=Tkinter.LEFT)
 
 
 # Class name Category selection Check Box creation
 
 cataegory = Tkinter.IntVar()
-Tkinter.Checkbutton(m, text="Display Class Name alone", variable=cataegory).pack(anchor=Tkinter.W)
+Tkinter.Checkbutton(topframe, text="Display Class Name alone", variable=cataegory).pack(side=Tkinter.LEFT)
 
 
 # Logs Type ( E - Error, W - Warning, D - Debug, I - Info, V - Verbose , ALL ) Category selection 
 # Radio box creation, packing and default option selection.
 
 logtype= Tkinter.StringVar()
-Tkinter.Radiobutton(m, 
+Tkinter.Radiobutton(topframe, 
               text="E",     # E - Error Category Radio Button 
               padx = 20, 
               variable=logtype, 
-              value='E').pack(anchor=Tkinter.W)
-Tkinter.Radiobutton(m, 
+              value='E').pack(side=Tkinter.LEFT)
+Tkinter.Radiobutton(topframe, 
               text="W",     # W - Warning Category Radio Button 
               padx = 20, 
               variable=logtype, 
-              value='W').pack(anchor=Tkinter.W)
-Tkinter.Radiobutton(m, 
+              value='W').pack(side=Tkinter.LEFT)
+Tkinter.Radiobutton(topframe, 
               text="D",     # D - Debug Category Radio Button  
               padx = 20, 
               variable=logtype, 
-              value='D').pack(anchor=Tkinter.W)
-Tkinter.Radiobutton(m, 
+              value='D').pack(side=Tkinter.LEFT)
+Tkinter.Radiobutton(topframe, 
               text="I",     # I - Info Category Radio Button 
               padx = 20, 
               variable=logtype, 
-              value='I').pack(anchor=Tkinter.W)
-Tkinter.Radiobutton(m, 
+              value='I').pack(side=Tkinter.LEFT)
+Tkinter.Radiobutton(topframe, 
               text="V",     # D - Debug Category Radio Button 
               padx = 20, 
               variable=logtype, 
-              value='V').pack(anchor=Tkinter.W)
-R1=Tkinter.Radiobutton(m, 
+              value='V').pack(side=Tkinter.LEFT)
+R1=Tkinter.Radiobutton(topframe, 
               text="All",     # V - Verbose Category Radio Button 
               padx = 20, 
               variable=logtype, 
               value='all')
-R1.pack(anchor=Tkinter.W)
+R1.pack(side=Tkinter.LEFT)
 R1.select()
+
+
+bottomframe = Tkinter.Frame(m)
+bottomframe.pack( side = Tkinter.BOTTOM )
 
 # Log Area Label creation and packing to the view area
 
-logarealabel = Tkinter.Label(m,justify = Tkinter.LEFT)
-logarealabel.pack()
+logarealabel = Tkinter.Label(bottomframe,justify = Tkinter.LEFT)
+logarealabel.config(text = "Log Area")
+logarealabel.pack(anchor=Tkinter.W)
 
 # Log Area creation and packing to the view area
 
-logarea = Tkinter.Text(m,wrap=Tkinter.WORD,width=200, height= 20)
+logarea = Tkinter.Text(bottomframe,wrap=Tkinter.WORD,width=200, height= 20, borderwidth=2, relief=Tkinter.GROOVE)
 logarea.pack(fill="none", expand=Tkinter.TRUE)
 
 m.mainloop()
